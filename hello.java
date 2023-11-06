1f2b64b83c90439b99b7477160e359230c820730
janith2021 <janithheshara1998@gmail.com>
2023-11-06 16:50:54 +0530

    Java files are added

---
+++ hello.java
@@ -0,0 +1,21 @@
+public class hello {
+    protected String name = "Janith Heshara";
+    public String age =  "25";
+    private String village = "Matara";
+
+    public static void main(String[] args) {
+        hello hi = new hello();
+        hi.name = "25";
+        System.out.println(hi.name);
+        System.out.println(hi.age);
+    }
+
+    // public static void main(String[] args) {
+    //     System.out.println("Hello From Janith Heshara");
+    //     // int i = 0;
+    //     for(int i=1;i<=100;i++){
+    //         System.out.println(i);
+    //     }
+        
+    // }
+}
---
+++ hi.java
@@ -0,0 +1,8 @@
+public class hi{
+
+    public static void main(String[] args) {
+        hello hi = new hello();
+        System.out.println(hi.age);
+    }
+    
+}
